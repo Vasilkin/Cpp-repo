1a50c14f6e6ba254632c9a6e24046aa428b22b46
Vasilkin <43407338+Vasilkin@users.noreply.github.com>
2020-08-11 00:12:55 +0300

    добавил итератор it_2

--- def_vec_set_it.cpp
+++ def_vec_set_it.cpp
@@ -25,6 +25,9 @@ int main()
     auto it = find_if(langs.begin(), langs.end(), [](const std::string& lang) {return lang[0] == 'C';});
     std::cout << it - langs.begin() << std::endl;
     // итератор для нахождения элемента по условию и позицию этого элемента
-    
+
+    std::set<int> s1{1, 6, 8, 9};
+    auto it_2 = s1.find(8);
+    PrintRange(next(it_2), s1.end());
     return 0;
 }
